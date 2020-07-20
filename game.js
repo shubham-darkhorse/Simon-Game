d9b9c801ddb0faad7196b5bb6f264dddd40745c9
shubham <smanohara2@gmail.com>
2020-07-20 16:05:10 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,81 @@
+
+var gamePattern=[];
+var buttonColours=["red","blue","green","yellow"];
+var userClickedPattern=[];
+var started=false;
+var level = 0;
+
+
+$(".btn").click(function(){
+  var userChosenColour=$(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence(){
+  userClickedPattern = [];
+  level++;
+  var randomNumber=Math.floor((Math.random()*4));
+  var randomChoosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChoosenColour);
+
+  $("#"+randomChoosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChoosenColour);
+}
+
+function playSound(name){
+  var audio=new Audio("sounds/"+name+".mp3")
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keypress(function(){
+  if(!started){
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    }
+
+  else {
+      var audio=new Audio("sounds/wrong.mp3");
+      audio.play();
+      $("body").addClass("game-over");
+
+      setTimeout(function(){
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      startOver();
+    }
+
+
+}
+
+function startOver(){
+  level = 0;
+  var gamePattern=[];
+  started=false;
+};
